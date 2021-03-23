9b8b914908982a790bf06523da125b014e560de0
jingyuan <343076881@qq.com>
2021-03-23 17:39:19 +0800

    Use SDK v1.0.1

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,60 +1,81 @@
+var useepay
 $(function () {
-  var useepay = UseePay({
+  useepay = UseePay({
     env: 'sandbox',
     layout: 'multiLine',
     locale: window.navigator.language,
   })
   useepay.mount(document.getElementById('cardElement'))
 
+  useepay.on('change', function (valid, code, message) {
+    if (valid) {
+      $('#payBtn').attr('disabled', false)
+      $('#errorTip').text('All input fields valid')
+    } else {
+      $('#payBtn').attr('disabled', true)
+      $('#errorTip').text(message)
+    }
+  })
+
   $('#payBtn')
     .off('click')
     .on('click', function () {
-      $('#payBtn').prop('disable', true)
-      $.post(
-        '/payment/token',
-        {
-          colorDepth: window && window.screen ? window.screen.colorDepth : '',
-          javaEnabled:
-            window && window.navigator ? window.navigator.javaEnabled() : false,
-          screenHeight: window && window.screen ? screen.height : '',
-          screenWidth: window && window.screen ? screen.width : '',
-          timeZoneOffset: new Date().getTimezoneOffset(),
-          language: window && window.navigator ? window.navigator.language : '',
-        },
-        function (resp) {
-          console.log(resp)
-          if (resp.success) {
-            useepay.confirm(resp.data.token, function (data) {
-              $('#payBtn').prop('disable', false)
-              if (data.success) {
-                $.post(
-                  '/payment/result',
-                  {
-                    result: data.data,
-                  },
-                  function (resp) {
-                    if (resp.success) {
-                      alert(
-                        'Transaction: ' +
-                          resp.data.transactionStatus +
-                          '\n' +
-                          'Message:' +
-                          resp.data.message,
-                      )
-                    } else {
-                      alert(resp.message)
-                    }
-                  },
-                )
-              } else {
-                // error occurred
-                alert(data.message)
-              }
-            })
-          } else {
-            $('#payBtn').prop('disable', false)
-          }
-        },
-      )
+      $('#payBtn').attr('disable', true)
+      useepay.validate(function (valid, code, message) {
+        if (valid) {
+          pay()
+        } else {
+          $('#errorTip').text(message)
+        }
+      })
     })
 })
+
+function pay() {
+  $.post(
+    '/payment/token',
+    {
+      colorDepth: window && window.screen ? window.screen.colorDepth : '',
+      javaEnabled:
+        window && window.navigator ? window.navigator.javaEnabled() : false,
+      screenHeight: window && window.screen ? screen.height : '',
+      screenWidth: window && window.screen ? screen.width : '',
+      timeZoneOffset: new Date().getTimezoneOffset(),
+      language: window && window.navigator ? window.navigator.language : '',
+    },
+    function (resp) {
+      console.log(resp)
+      if (resp.success) {
+        useepay.confirm(resp.data.token, function (data) {
+          $('#payBtn').prop('disable', false)
+          if (data.success) {
+            $.post(
+              '/payment/result',
+              {
+                result: data.data,
+              },
+              function (resp) {
+                if (resp.success) {
+                  alert(
+                    'Transaction: ' +
+                      resp.data.transactionStatus +
+                      '\n' +
+                      'Message:' +
+                      resp.data.message,
+                  )
+                } else {
+                  alert(resp.message)
+                }
+              },
+            )
+          } else {
+            // error occurred
+            alert(data.message)
+          }
+        })
+      } else {
+        $('#payBtn').prop('disable', false)
+      }
+    },
+  )
+}
